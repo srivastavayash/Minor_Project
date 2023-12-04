43aac5ad64dce0eafc90f5b396608693f4ed5278
Yash Srivastava <srivastavayash017@gmail.com>
2023-12-05 02:42:26 +0530

    Changes

--- virtualenv/Scripts/activate.bat
+++ virtualenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Parth\Desktop\VS Studio\Stock-Prediction-System-Application\virtualenv
+set VIRTUAL_ENV=C:\Users\Sriva\Documents\MINOR PROJECT\StockMl\virtualenv
 
 if not defined PROMPT set PROMPT=$P$G
 
